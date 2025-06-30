05ed961701dc8521f5a74b5c6414c5fd58dfb8b1
Mayuri-kub-26 <mmayu8497@gmail.com>
2025-06-30 16:25:52 +0530

    Add files via upload

---
+++ Project file/main.py
@@ -0,0 +1,8 @@
+import cv2 as cv2web_cam = cv.VideoCapture(0)
+while True:
+    isTrue, frame= web_cam.read
+    cv.imshow("Video",frame)
+    if cv.waitKey(20)&0xFF==ord('f'):
+        break
+web_cam.release()
+cv.destroyAllWindows()
